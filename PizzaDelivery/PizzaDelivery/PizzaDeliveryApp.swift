363fa929c15e75643318f23a9007d852b866b9cb
boikostas <boikostas3@gmail.com>
2024-10-09 14:32:42 +0300

    project created

---
+++ PizzaDelivery/PizzaDelivery/PizzaDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaDeliveryApp.swift
+//  PizzaDelivery
+//
+//  Created by Stas Boiko on 09.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct PizzaDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
